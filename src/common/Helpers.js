92691def72d85505c5eb19c670461a01f91b67e4
unknown <dipakbarman080@gmail.com>
2023-12-11 19:40:30 +0530

    11-12 2

--- src/common/Helpers.js
+++ src/common/Helpers.js
@@ -4,8 +4,8 @@ export const admin_url = app_url + 'admin/';
 export const organizer_url = app_url + 'organizer/';
 export const customer_url = app_url + 'customer/';
 
-// export const apiurl = 'https://nodejsapidev.vercel.app/api/v1/';
-export const apiurl = 'http://localhost:5001/api/v1/';
+export const apiurl = 'https://nodejsapidev.vercel.app/api/v1/';
+// export const apiurl = 'http://localhost:5001/api/v1/';
 export function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
